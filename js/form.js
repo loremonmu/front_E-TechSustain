05ea40dea940b1d012aa15284b514b1d82f015af
Sarah <75185314+sarisp3260@users.noreply.github.com>
2024-01-26 23:55:03 -0500

    Formulario contacto  completo, envia a correo personal de la empresa :art:

--- js/form.js
+++ js/form.js
@@ -1,11 +1,13 @@
 const formulario = document.getElementById('contact-form');
 const inputs = document.querySelectorAll('#contact-form input, #contact-form textarea');
 
+console.log(inputs);
+
 const expresiones = {
   name: /^[a-zA-ZÀ-ÿ\s]+(?:\s[a-zA-ZÀ-ÿ\s]+)*(?:\s\d{1,2})?$/,
   email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
   phone: /^\d{7,14}$/,
-  message: /^[a-zA-Z0-9\s.,?!¡¿()-]{1,1000}$/ 
+  message: /^[a-zA-Z0-9\s.,?!¡¿()-]{1,1000}$/
 };
 
 const campos = {
@@ -59,16 +61,35 @@ formulario.addEventListener('submit', (e) => {
   e.preventDefault();
 
   if (campos.name && campos.email && campos.phone && campos.message) {
-    formulario.reset();
 
-    document.getElementById('form-message-success').classList.add('form-message-success-active');
-    setTimeout(() => {
-      document.getElementById('form-message-success').classList.remove('form-message-success-active');
-    }, 5000);
+    emailjs.init('6Y6Zzk4wdHm33LgFN');
+    const serviceID = 'service_93ayrh8';
+    const templateID = 'template_1u133p8';
+    const templateParams = {
+      name: inputs[0].value,
+      email: inputs[1].value,
+      phone: inputs[2].value,
+      message: inputs[3].value,
+    };
+
+    emailjs.send(serviceID, templateID, templateParams)
+      .then(function (response) {
+        document.getElementById('form-message-success').classList.add('form-message-success-active');
+        setTimeout(() => {
+          document.getElementById('form-message-success').classList.remove('form-message-success-active');
+        }, 5000);
+      }, function (error) {
+        console.error('Error al enviar el correo:', error);
+      });
+
+    formulario.reset();
 
     document.querySelectorAll('.field-form-correct').forEach((icono) => {
       icono.classList.remove('field-form-correct');
     });
+
+
+
   } else {
     document.getElementById('form-message').classList.add('form-message-active');
   }
